66d093fd33f2e60b32a16ac9fa29c35e73b3fec5
Karim Shaloh <shal0054@algonquinlive.com>
2021-01-11 13:54:38 -0400

    excerise complete

--- helloStudents.js
+++ helloStudents.js
@@ -7,3 +7,14 @@
 
 // 3. Print out the number of last names starting with the letter D
 // e.g. Count of last names starting with D is 1
+
+const students = require('./students.json');
+
+function sayHello() {
+   const targetStudents = students.filter(student => student.lastName[0] === 'D');
+   targetStudents.forEach(student => {
+      console.log(`Hello ${student.firstName} ${student.lastName}`);
+   });
+}
+
+sayHello();
